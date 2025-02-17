f0fc58cd121b75cbe5cd33d3de794c1df5d0ad4c
Carlos Cruz Jr. <ccruzjr2005@gmail.com>
2025-02-16 19:36:08 -0500

    changing package file

--- calculator.js
+++ calculator.js
@@ -12,11 +12,51 @@ const num1 = parseFloat(process.argv[2]);
 const operation = (process.argv[3]).toLowerCase();
 const num2 = parseFloat(process.argv[4]);
 
-let result = `Solving ${num1} ${operation} ${num2}`;
 
 // Implementation goes here
 // if you get an error, exit with an error of 1 by calling process.exit(1);
 
+//Code checks for valid input
+if (isNaN(num1) || isNaN(num2)) {
+  console.error("invalid number");
+  process.exit(1);
+}
+
+//Divide by 0 Code
+if (operation === 'divide' && num2 === 0) {
+  console.error("invalid number");
+  process.exit(1);
+}
+
+//Calculator code:
+
+let result;
+
+switch (operation) {
+  case 'add':
+    result = add(num1, num2);
+    break;
+  case 'subtract':
+    result = subtract(num1, num2);
+    break;
+  case 'multiply':
+  case 'times':
+    result = times(num1, num2);
+    break;
+  case 'divide':
+    result = divide(num1, num2);
+    break;
+  case 'modulus':
+  case 'remainder':
+    result = modulus(num1, num2);
+    break;
+  case 'power':
+    result = power(num1, num2);
+    break;
+  default:
+    console.error("unknown operation");
+    process.exit(1);
+}
 
 console.log("result: ", result);
 process.exit(0);
